a6ceb06872804729463ca870ddee98ddfc5e559e
Sara0614 <euniceisnot10@gmail.com>
2025-02-20 14:37:13 -0800

    I made a scripting assignment on destorying
    I made a scripting assignment on destorying a game object and plane.

---
+++ ScriptingProjectP8Velasquez/Assets/Scripts/LinearInter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInter : MonoBehaviour
+{
+    /*
+     
+    //In this case, result =4
+
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector 3 (5f, 6f, 7f);
+    //Here result = (4, 5, 6)
+    Vector3 result = Vector3.ZLerp (from,to, 0.75f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensisty, 8f 0.5f);
+    light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f *Time.deltaTime);
+    }
+    */
+}
